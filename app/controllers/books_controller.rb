be2b4af5c9e4ce8ff3663fa159d759493adde2b0
Joaquin Fischer <joaquin.fischer@gmail.com>
2021-07-20 14:58:00 +0200

    Book animation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:edit, :update, :destroy]
 
   def index
     @books = Book.all
