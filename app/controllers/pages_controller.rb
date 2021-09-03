109da070287819b2f64672be859f4eaed2df9fdf
Joaquin Fischer <83013180+jfis223@users.noreply.github.com>
2021-09-03 17:34:35 +0200

    Minor fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :biografia]
+  skip_before_action :authenticate_user!, only: [ :home, :biografia, :privacy_policy]
 
   def home
   end
 
   def biografia
+  end
+
+  def privacy_policy
   end
 end
