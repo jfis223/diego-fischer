b2da9db7d881032eb535881094d1f0b911a734d3
Joaquin Fischer <joaquin.fischer@gmail.com>
2022-09-02 15:14:01 +0200

    Changed domain for Mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'diego-fischer.herokuapp.com/', # UPDATE THIS VALUE WITH YOUR OWN APP
+  :domain         => 'diego-fischer.fly.dev/', # UPDATE THIS VALUE WITH YOUR OWN APP
   :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
