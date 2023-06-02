f6358bd301b8e56171e071c17e1d3b148d648d48
Joaquin Fischer <joaquin.fischer@gmail.com>
2023-06-02 11:23:01 -0300

    fix: several styling and permissions bugfixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'biografia', to: 'pages#biografia'
-  get 'privacy-policy', to: 'pages#privacy-policy'
+  get 'privacy-policy', to: 'pages#privacy_policy'
   resources :slides
   resources :contacts, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
